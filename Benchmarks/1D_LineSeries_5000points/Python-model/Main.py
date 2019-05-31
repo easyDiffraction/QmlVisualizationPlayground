13a56d312e4f2106a279ca9e3e02a744f65d851a
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-05-31 13:18:27 +0200

    Update Python model

---
+++ Benchmarks/1D_LineSeries_5000points/Python-model/Main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import random
+from datetime import datetime
+from PySide2.QtCore import QUrl, Qt
+from PySide2.QtGui import QStandardItem, QStandardItemModel
+from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    timeNow = datetime.now()
+
+    # create application
+    app = QApplication(sys.argv)
+
+    # load qml
+    engine = QQmlApplicationEngine(QUrl.fromLocalFile(os.path.join(os.path.dirname(sys.argv[0]), "Main.qml")))
+
+    # create model for plot
+    nMax = 5000
+    dataModel = QStandardItemModel(nMax+1, 2) # nMax+1 rows and 2 columns
+    for row in range(dataModel.rowCount()):
+        item1 = QStandardItem()
+        item1.setData(row/nMax, Qt.DisplayRole)
+        item2 = QStandardItem()
+        item2.setData(random.random(), Qt.DisplayRole)
+        dataModel.setItem(row, 0, item1)
+        dataModel.setItem(row, 1, item2)
+
+    # register model for access from qml
+    engine.rootContext().setContextProperty("dataModel", dataModel);
+
+    print("Performance:", datetime.now() - timeNow)
+
+    # ensure that the QML was loaded by checking if a root object was created
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    # close the program after the Qt Event loop is over
+    sys.exit(app.exec_())
